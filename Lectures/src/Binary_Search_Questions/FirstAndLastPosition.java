c681a2680757c8654a0ebdd03afe2a8b1373f293
Atharv Vani <atharvavani110@gmail.com>
2022-08-06 20:29:18 +0530

    Lectures : Question 4 : First and Last Position ofelement in sorted array

--- Lectures/src/Binary_Search_Questions/FirstAndLastPosition.java
+++ Lectures/src/Binary_Search_Questions/FirstAndLastPosition.java
@@ -9,6 +9,7 @@ public class FirstAndLastPosition {
     public static void main(String[] args) {
         int[] nums = {5,7,7,8,8,10};
         int target = 8;
+//      int target = 6; output = [-1, -1];  
         int[] ans = searchRange(nums, target);
         System.out.println(Arrays.toString(ans));
     }
