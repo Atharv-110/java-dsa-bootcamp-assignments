6baca67d434708558a6284595344bbdc14eb501c
Atharv Vani <atharvavani110@gmail.com>
2022-08-06 20:28:17 +0530

    Lectures : Question 4 : First and Last Position ofelement in sorted array

--- Lectures/src/Binary_Search_Questions/FirstAndLastPosition.java
+++ Lectures/src/Binary_Search_Questions/FirstAndLastPosition.java
@@ -1,12 +1,48 @@
-//
-//
+// Find First and Last Position of Element in Sorted Array
+// https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/submissions/
 
 package Binary_Search_Questions;
 
-/**
- *
- * @author hp
- */
+import java.util.Arrays;
+
 public class FirstAndLastPosition {
-    
+    public static void main(String[] args) {
+        int[] nums = {5,7,7,8,8,10};
+        int target = 8;
+        int[] ans = searchRange(nums, target);
+        System.out.println(Arrays.toString(ans));
+    }
+
+    static int[] searchRange(int[] nums, int target) {
+        int[] ans = {-1, -1};
+        int start = search(nums, target, true);
+        int end = search(nums, target, false);
+        ans[0] = start;
+        ans[1] = end;
+        return ans;
+    }
+
+    static int search(int[] nums, int target, boolean b) {
+        int start = 0, ans = -1;
+        int end = nums.length - 1;
+        while (start <= end) {    
+            int mid = start + (end - start) / 2;
+            if (target < nums[mid]) {
+                end = mid - 1;
+            }
+            else if (target > nums[mid]) { 
+                start = mid + 1;
+            }
+            else {
+                ans = mid;
+                if (b) {
+                    end = mid - 1;
+                }
+                else  {
+                    start = mid + 1;
+                }
+            }
+        }
+        return ans;
+    }
 }
