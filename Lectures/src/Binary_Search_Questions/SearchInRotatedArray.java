afe7a45b49e55a7c98f09cd7ec0126673762f2ee
Atharv Vani <atharvavani110@gmail.com>
2022-08-11 01:39:48 +0530

    Lectures : Question 8 : Search in Rotated Sorted Array with Duplicates

--- Lectures/src/Binary_Search_Questions/SearchInRotatedArray.java
+++ Lectures/src/Binary_Search_Questions/SearchInRotatedArray.java
@@ -5,9 +5,10 @@ package Binary_Search_Questions;
 
 public class SearchInRotatedArray {
     public static void main(String[] args) {
-        int[] nums = {4,5,6,7,0,1,2};
-        int target = 0;
-        System.out.println(search(nums, target));
+//        int[] nums = {6,7,3,4,5,5,6};
+//        int target = 0;
+//        System.out.println(search(nums, target));
+//        System.out.println(findPivotWithDuplicates(nums));
     }
 
     static int search(int[] nums, int target) {
@@ -42,6 +43,37 @@ public class SearchInRotatedArray {
             }
             else {
                 start = mid + 1;
+            }
+        }
+        return -1;
+    }
+    
+    static int findPivotWithDuplicates(int[] nums){
+        int start = 0;
+        int end = nums.length - 1;
+        while (start <= end) {            
+            int mid = start + (end - start) / 2;
+            if (mid < end && nums[mid] > nums[mid + 1]) {
+                return mid;
+            }
+            if (mid > start && nums[mid] < nums[mid - 1]) {
+                return mid - 1;
+            }
+            if(nums[start] == nums[mid] && nums[mid] == nums[end]){
+                if (start < end && nums[start] > nums[start + 1]) {
+                    return start;
+                }
+                start++;
+                if (start > end && nums[end] < nums[end - 1]) {
+                    return end - 1;
+                }
+                end--;
+            }
+            else if (nums[start] < nums[mid] || (nums[start] == nums[mid] && nums[mid] > nums[end])) {
+                start = mid + 1;
+            }
+            else {
+                end = mid - 1;
             }
         }
         return -1;
