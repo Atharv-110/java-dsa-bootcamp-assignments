c278e1c85c23b8fb4b294991a511c8bc5c3f7111
Atharv Vani <atharvavani110@gmail.com>
2022-08-16 20:02:37 +0530

    Lectures : Sorting : Bubble Sort Code Added

--- Lectures/src/Sorting/bubble.java
+++ Lectures/src/Sorting/bubble.java
@@ -1,13 +1,32 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+// Bubble Sort
+
 package Sorting;
 
-/**
- *
- * @author hp
- */
+import java.util.Arrays;
+
 public class bubble {
-    
+    public static void main(String[] args) {
+        int[] arr = {3, 1, 5, 4, 2};
+//        bubble(arr);
+        bubbleSorting(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void bubbleSorting(int[] arr) {
+        boolean swapped;
+        for (int i = 0; i < arr.length; i++) {
+            swapped = false;
+            for (int j = 1; j < arr.length - i; j++) {
+                if (arr[j] < arr[j-1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j - 1];
+                    arr[j - 1] = temp;
+                    swapped = true;
+                }
+            }
+            if (!swapped) {
+                break;
+            }
+        }
+    }
 }
