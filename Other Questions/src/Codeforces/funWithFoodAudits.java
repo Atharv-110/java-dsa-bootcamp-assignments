b21e798c45a42a18b946b6e8a9f71f14d29b9df3
Atharv Vani <atharvavani110@gmail.com>
2023-03-24 00:37:42 +0530

    Other Questions>CodeForces>Que added

---
+++ Other Questions/src/Codeforces/funWithFoodAudits.java
@@ -0,0 +1,19 @@
+// A. Fun with Food Audits
+// https://codeforces.com/gym/104237/problem/A
+package Codeforces;
+
+import java.util.Scanner;
+
+public class funWithFoodAudits {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] garbage = new int[4];
+        int sum = 0;
+        for (int i = 0; i < garbage.length; i++) {
+            garbage[i] = sc.nextInt();
+            sum += garbage[i];
+        }
+        System.out.println(sum-24);
+        
+    }
+}
