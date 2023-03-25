9d2b060c46244211b6a44fabb4dce9afd66454fa
Atharv Vani <atharvavani110@gmail.com>
2023-03-25 21:45:24 +0530

    Assignments>Other Questions>Codeforces>Question added

--- Other Questions/src/Codeforces/funWithFoodAudits.java
+++ Other Questions/src/Codeforces/funWithFoodAudits.java
@@ -14,6 +14,11 @@ public class funWithFoodAudits {
             sum += garbage[i];
         }
         System.out.println(sum-24);
+//        int a = sc.nextInt();
+//        int b = sc.nextInt();
+//        int c = sc.nextInt();
+//        int d = sc.nextInt();
+//        System.out.println((a+b+c+d)-(6*4));
         
     }
 }
