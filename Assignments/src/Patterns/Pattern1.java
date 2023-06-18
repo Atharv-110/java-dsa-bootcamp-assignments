96fe72ba370f52dfec68b46a21be67e79053c49e
Atharv Vani <atharvavani110@gmail.com>
2023-06-18 17:21:34 +0530

    Assignments>Patterns>Parttern1>added

---
+++ Assignments/src/Patterns/Pattern1.java
@@ -0,0 +1,18 @@
+// Assignments: Pattern 1
+
+package Patterns;
+
+public class Pattern1 {
+    public static void main(String[] args) {
+        pattern1(5);
+    }
+
+    private static void pattern1(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
