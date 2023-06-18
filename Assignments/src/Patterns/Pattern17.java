c839b0a6834042467f52fcf45ca6dc8ec14624d7
Atharv Vani <atharvavani110@gmail.com>
2023-06-19 01:20:42 +0530

    Assignments>Patterns>Parttern1>added

---
+++ Assignments/src/Patterns/Pattern17.java
@@ -0,0 +1,25 @@
+// Assignments pattern 17
+
+package Patterns;
+
+public class Pattern17 {
+    public static void main(String[] args) {
+        pattern17(4);
+    }
+
+    private static void pattern17(int n) {
+        for (int row = 1; row <= 2 * n; row++) {
+            int c = row > n ? 2*n-row : row ;
+            for (int s = 1; s <= n-c; s++) {
+                System.out.print("  ");
+            }
+            for (int col = c; col >= 1 ; col--) {
+                System.out.print(col + " ");
+            }
+            for (int col = 2; col <= c; col++) {
+                System.out.print(col +" ");
+            }
+            System.out.println();
+        }
+    }
+}
