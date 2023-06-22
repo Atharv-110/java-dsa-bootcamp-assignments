c8efb2ead9e9dadd0ffca91abdebf16c856a2e52
Atharv Vani <atharvavani110@gmail.com>
2023-06-22 11:59:44 +0530

    Lectures>Recursion>Example2>added

---
+++ Lectures/src/Recursion/FibonacciNum.java
@@ -0,0 +1,18 @@
+// Example Fibonacci Number
+//
+
+package Recursion;
+
+public class FibonacciNum {
+    public static void main(String[] args) {
+        int ans = fibo(7);
+        System.out.println(ans);
+    }
+
+    private static int fibo(int n) {
+        if(n < 2) {
+            return n;
+        }
+        return fibo(n-1) + fibo(n-2);
+    }
+}
