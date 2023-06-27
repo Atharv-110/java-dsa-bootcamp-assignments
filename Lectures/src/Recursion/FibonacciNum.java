7a418bb6d97c256a8122c55a097fa994daa930b0
Atharv Vani <atharvavani110@gmail.com>
2023-06-28 03:44:29 +0530

    Fibonacci number using formula

--- Lectures/src/Recursion/FibonacciNum.java
+++ Lectures/src/Recursion/FibonacciNum.java
@@ -5,7 +5,7 @@ package Recursion;
 
 public class FibonacciNum {
     public static void main(String[] args) {
-        int ans = fibo(7);
+        int ans = fiboFormula(6);
         System.out.println(ans);
     }
 
@@ -14,5 +14,9 @@ public class FibonacciNum {
             return n;
         }
         return fibo(n-1) + fibo(n-2);
+    }
+
+    private static int fiboFormula(int n) {
+        return (int)((Math.pow(((1 + Math.sqrt(5))/2),n) - (Math.pow(((1 - Math.sqrt(5))/2), n))) / Math.sqrt(5));
     }
 }
