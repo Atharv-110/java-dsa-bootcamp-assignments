f392725e70dfe48da8a4441d06359faa7bd0b6e9
Atharv Vani <atharvavani110@gmail.com>
2023-07-05 00:38:58 +0530

    Assignments>Searching>Medium>Leetcode81>added

--- Assignments/src/Searching/Medium/RotatedSortedArrayTwo.java
+++ Assignments/src/Searching/Medium/RotatedSortedArrayTwo.java
@@ -1,13 +1,28 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+// Leetcode 81
+
 package Searching.Medium;
 
-/**
- *
- * @author hp
- */
+
 public class RotatedSortedArrayTwo {
-    
+    public boolean search(int[] nums, int target) {
+        int start = 0, end = nums.length-1;
+        while (start <= end) {            
+            if(nums[start] == target || nums[end] == target){
+                return true;
+            } else if (target > nums[start]) {
+                while (start < end && nums[start+1]==nums[start]) {                    
+                    start++;
+                }
+                start++;
+            } else if (target < nums[end]) {
+                while (start < end && nums[end-1]==nums[end]) {                    
+                    end--;
+                }
+                end--;
+            } else {
+                break;
+            }
+        }
+        return false;
+    }
 }
