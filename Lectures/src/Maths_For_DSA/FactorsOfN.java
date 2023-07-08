da1e1515713dc507753abb1ab1ef065d00c26e6e
Atharv Vani <atharvavani110@gmail.com>
2023-07-08 23:18:38 +0530

    Lectures>Maths_For_DSA>FactorsOfN>Added

---
+++ Lectures/src/Maths_For_DSA/FactorsOfN.java
@@ -0,0 +1,28 @@
+// Find the factors of a number given using optimised appch.
+
+package Maths_For_DSA;
+
+import java.util.ArrayList;
+
+public class FactorsOfN {
+    public static void main(String[] args) {
+        factors(36);
+    }
+
+    private static void factors(int n) {
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i = 1; i <= Math.sqrt(n); i++) {
+            if(n % i == 0) {
+                if(n/i == i) {
+                    System.out.print(i + " ");
+                } else {
+                    System.out.print(i + " ");
+                    list.add(n/i);
+                }
+            }
+        }
+        for (int i = list.size()-1; i >= 0; i--) {
+            System.out.print(list.get(i) + " ");
+        }
+    }
+}
