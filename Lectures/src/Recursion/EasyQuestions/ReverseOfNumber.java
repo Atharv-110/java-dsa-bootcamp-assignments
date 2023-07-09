7974550b8d98c57729e49970901bf7a510582156
Atharv Vani <atharvavani110@gmail.com>
2023-07-10 03:52:58 +0530

    Lectures>Recursion>EasyQuestions>ReverseNumMethod2Pure>added

--- Lectures/src/Recursion/EasyQuestions/ReverseOfNumber.java
+++ Lectures/src/Recursion/EasyQuestions/ReverseOfNumber.java
@@ -12,9 +12,27 @@ public class ReverseOfNumber {
         sum = sum * 10 + rem;
         reverse1(n/10);
     }
-    public static void main(String[] args) {
-        reverse1(1234);
-        System.out.println(sum);
+    
+//    pure recursive method
+    static int reverse2(int n) {
+        int digits = (int)(Math.log10(n))+1;
+        return helperFun(n, digits);
     }
+    
+    private static int helperFun(int n, int digits) {
+        if(n%10 == n) {
+            return n;
+        }
+        int rem = n % 10;
+        return rem * (int)(Math.pow(10, digits-1)) + helperFun(n/10, digits-1);
+    }
+    
+//    Main Method
+    public static void main(String[] args) {
+//        reverse1(1234);
+        System.out.println(reverse2(12355));
+    }
+
+    
     
 }
