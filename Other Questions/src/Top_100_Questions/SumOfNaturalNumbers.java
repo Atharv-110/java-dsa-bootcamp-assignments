bddacc0e31756ff1b66cc279bab0950f64c2f5db
Atharv Vani <atharvavani110@gmail.com>
2023-07-14 02:24:14 +0530

    Top 100 Codes

---
+++ Other Questions/src/Top_100_Questions/SumOfNaturalNumbers.java
@@ -0,0 +1,25 @@
+// Program to sum the n natural numbers
+
+package Top_100_Questions;
+
+public class SumOfNaturalNumbers {
+    public static void main(String[] args) {
+        System.out.println(sumIterative(5));
+        System.err.println(sumRecursive(5));
+    }
+    
+    private static int sumIterative(int n) {
+        int sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+    
+    private static int sumRecursive(int n) {
+        if (n <= 1) {
+            return 1;
+        }
+        return n + sumRecursive(n-1);
+    }
+}
