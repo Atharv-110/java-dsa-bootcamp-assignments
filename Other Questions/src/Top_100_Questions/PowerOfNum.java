390becf61969367a68c9ecb88ca70696f6f19374
Atharv Vani <atharvavani110@gmail.com>
2023-07-19 10:01:59 +0530

    top 100 questions

---
+++ Other Questions/src/Top_100_Questions/PowerOfNum.java
@@ -0,0 +1,13 @@
+// Power of a number
+
+package Top_100_Questions;
+
+public class PowerOfNum {
+    public static void main(String[] args) {
+        System.out.println(power(2, 3));
+    }
+    
+    private static double power(int num, int exp) {
+        return Math.pow(num, exp);
+    }
+}
