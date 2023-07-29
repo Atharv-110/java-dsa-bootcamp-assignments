3cecc023e2e5317f5aee6b144ec8af5fa3904cfd
Atharv Vani <atharvavani110@gmail.com>
2023-07-29 23:15:34 +0530

    Lectures>Recursion>Subset_Subsequence_String>SkipAString>added

--- Lectures/src/Recursion/Subset_Subsequence_String/SkipAString.java
+++ Lectures/src/Recursion/Subset_Subsequence_String/SkipAString.java
@@ -1,13 +1,21 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+// Write a program to skip a particular string and get the remaining string as it is
 package Recursion.Subset_Subsequence_String;
 
-/**
- *
- * @author hp
- */
 public class SkipAString {
+    public static void main(String[] args) {
+        System.out.println(skipString("bcdapplefg"));
+    }
     
+    private static String skipString(String str) {
+        if (str.isEmpty()) {
+            return "";
+        }
+        
+        if (str.startsWith("apple")) {
+            return skipString(str.substring(5));
+        }
+        else {
+            return str.charAt(0) + skipString(str.substring(1));
+        }
+    }
 }
